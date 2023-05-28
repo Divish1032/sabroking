9df15f8f0cb98279bb561974c2af031473df9ccb
Divyansh Kumar <divyanshkumar@Divyanshs-MacBook-Pro.local>
2023-05-28 23:37:42 +0530

    version update

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-const LocalStrategy = require('passport-local').Strategy;
+const LocalStrategy = require("passport-local").Strategy;
 
-var fs = require('fs'); 
-var path = require('path'); 
-var multer = require('multer'); 
+var fs = require("fs");
+var path = require("path");
+var multer = require("multer");
 
-const GridFsStorage = require('multer-gridfs-storage');
-const Grid = require('gridfs-stream');
-const methodOverride = require('method-override');
+const GridFsStorage = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
+const methodOverride = require("method-override");
 
 const app = express();
 
@@ -22,7 +22,7 @@ app.set("view engine", "ejs");
 app.use(
   bodyParser.urlencoded({
     extended: true,
-  })  
+  })
 );
 
 app.use(
@@ -36,7 +36,6 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 const mongoURI = "mongodb+srv://vaibhav412:v12345a@cluster0.if63g.mongodb.net/sabrokingDB";
 
 const options = {
@@ -48,7 +47,7 @@ const options = {
   poolSize: 10, // Maintain up to 10 socket connections
   serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  family: 4 // Use IPv4, skip trying IPv6
+  family: 4, // Use IPv4, skip trying IPv6
 };
 
 mongoose.connect(mongoURI, options);
@@ -57,15 +56,15 @@ mongoose.set("useCreateIndex", true);
 
 const conn = mongoose.createConnection(mongoURI, options);
 
-mongoose.set('useUnifiedTopology', true);
+mongoose.set("useUnifiedTopology", true);
 
 // Init gfs
 let gfs;
 
-conn.once('open', () => {
+conn.once("open", () => {
   // Init stream
   gfs = Grid(conn.db, mongoose.mongo);
-  gfs.collection('uploads');
+  gfs.collection("uploads");
 });
 
 // Create storage engine
@@ -75,12 +74,12 @@ const storage = new GridFsStorage({
     return new Promise((resolve, reject) => {
       const filename = file.originalname;
       const fileInfo = {
-          filename: filename,
-          bucketName: 'uploads'
+        filename: filename,
+        bucketName: "uploads",
       };
       resolve(fileInfo);
     });
-  }
+  },
 });
 const upload = multer({ storage });
 
@@ -92,7 +91,7 @@ const sabrokingSchema = new mongoose.Schema({
   role: Number,
   file_path: Array,
 });
- 
+
 sabrokingSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", sabrokingSchema);
@@ -111,39 +110,58 @@ passport.deserializeUser(function (id, done) {
 
 app.get("/", function (req, res) {
   var isAuthenticated = req.isAuthenticated();
-  res.render("home", {isAuthenticated: isAuthenticated});
+  res.render("home", { isAuthenticated: isAuthenticated });
 });
 
 // app.get("/about", function (req, res) {
 //   res.render("about");
 // });
 
-app.get('/formdownload',(req,res)=>{  
-    var path= __dirname + '/' + req.query.download;  
-    res.download(path);
+app.get("/formdownload", (req, res) => {
+  var path = __dirname + "/" + req.query.download;
+  res.download(path);
 });
 
 app.get("/dashboard", function (req, res) {
   if (req.isAuthenticated() && req.user.role == 0) {
-    User.findOne({
-      _id: req.user._id
-    }, function (err, user) {
-      res.render("userdashboard", {
-        user: user,
-      });
-    });
+    User.findOne(
+      {
+        _id: req.user._id,
+      },
+      function (err, user) {
+        res.render("userdashboard", {
+          user: user,
+        });
+      }
+    );
+  } else {
+    res.redirect("/");
+  }
+});
+
+app.get("/investor-corner", function (req, res) {
+  if (req.isAuthenticated() && req.user.role == 0) {
+    User.findOne(
+      {
+        _id: req.user._id,
+      },
+      function (err, user) {
+        res.render("investor", {
+          user: user,
+        });
+      }
+    );
   } else {
     res.redirect("/");
   }
 });
 
 app.get("/admin/dashboard", function (req, res) {
-  if (req.isAuthenticated()){
-    if(req.user.role == 1){
+  if (req.isAuthenticated()) {
+    if (req.user.role == 1) {
       User.find({}, function (err, users) {
         res.render("admindashboard", { users: users });
       });
-
     } else {
       res.redirect("/admin");
     }
@@ -153,186 +171,187 @@ app.get("/admin/dashboard", function (req, res) {
 });
 
 app.get("/admin/:userId", function (req, res) {
-
   const requestedUserId = req.params.userId;
-  if (req.isAuthenticated() && req.user.role == 1){
-  
-  User.findOne({
-    _id: requestedUserId
-  }, function (err, user) {
-    gfs.files.find().toArray((err, files) => {
-    res.render('uploadfile', { files: files, user: user });
-  });
-  });
-} else {
-  res.redirect("/admin");
-}
-
+  if (req.isAuthenticated() && req.user.role == 1) {
+    User.findOne(
+      {
+        _id: requestedUserId,
+      },
+      function (err, user) {
+        gfs.files.find().toArray((err, files) => {
+          res.render("uploadfile", { files: files, user: user });
+        });
+      }
+    );
+  } else {
+    res.redirect("/admin");
+  }
 });
 
-
-app.post('/admin/:userId',upload.array('file', 5),(req,res)=>{ 
-
+app.post("/admin/:userId", upload.array("file", 5), (req, res) => {
   const requestedUserId = req.params.userId;
-  if (req.isAuthenticated() && req.user.role == 1){
-  
-  for(i=0;i<req.files.length;i++){
-    var x= req.files[i].filename;    
-    User.findByIdAndUpdate(requestedUserId,
-      {$push: {file_path: x}},
-      {safe: true, upsert: true},
-      function(err, doc) {
-          if(err){
-          console.log(err);
-          }else{
-          console.log(doc);
+  if (req.isAuthenticated() && req.user.role == 1) {
+    for (i = 0; i < req.files.length; i++) {
+      var x = req.files[i].filename;
+      User.findByIdAndUpdate(
+        requestedUserId,
+        { $push: { file_path: x } },
+        { safe: true, upsert: true },
+        function (err, doc) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(doc);
           }
-      }
-  );  
-  }
-  res.redirect("/admin/" + requestedUserId); 
-
-} else {
+        }
+      );
+    }
+    res.redirect("/admin/" + requestedUserId);
+  } else {
     res.redirect("/admin");
   }
 });
 
 app.get("/download/:filename", (req, res) => {
+  if (req.isAuthenticated()) {
+    const filename = req.params.filename;
 
-  if (req.isAuthenticated()){
-
-  const filename = req.params.filename;
-
-  gfs.files.findOne({ filename: filename }, (err, file) => {
-    const readstream = gfs.createReadStream(file.filename);
-    readstream.pipe(res);
-    console.log(file);
-  });
- } else {
+    gfs.files.findOne({ filename: filename }, (err, file) => {
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+      console.log(file);
+    });
+  } else {
     res.redirect("/");
   }
 });
 
 app.get("/delete/:filename", (req, res) => {
-  if (req.isAuthenticated() && req.user.role == 1){
+  if (req.isAuthenticated() && req.user.role == 1) {
+    const filename = req.params.filename;
 
-  const filename = req.params.filename;
+    gfs.remove({ filename: filename, root: "uploads" }, (err, gridStore) => {
+      if (err) {
+        return res.status(404).json({ err: err });
+      }
+      User.findByIdAndUpdate(
+        req.query.userId,
 
-  gfs.remove({ filename: filename, root: 'uploads' }, (err, gridStore) => {
-    if (err) {
-      return res.status(404).json({ err: err });
-    }
-    User.findByIdAndUpdate(req.query.userId,
-
-      {$pull: {file_path: filename}},
-      {safe: true, upsert: true},
-      function(err, doc) {
-          if(err){
-          console.log(err);
-          }else{
+        { $pull: { file_path: filename } },
+        { safe: true, upsert: true },
+        function (err, doc) {
+          if (err) {
+            console.log(err);
+          } else {
             res.redirect("/admin/" + req.query.userId);
           }
-      })
-  });
-} else {
-  res.redirect("/admin");
-}
+        }
+      );
+    });
+  } else {
+    res.redirect("/admin");
+  }
 });
 
 app.get("/admin", function (req, res) {
-  if (req.isAuthenticated() && req.user.role == 1){
-    res.redirect('/admin/dashboard');
+  if (req.isAuthenticated() && req.user.role == 1) {
+    res.redirect("/admin/dashboard");
   } else {
-    if (req.isAuthenticated() && req.user.role == 0){
-      res.redirect('/dashboard');
+    if (req.isAuthenticated() && req.user.role == 0) {
+      res.redirect("/dashboard");
     } else {
       res.render("admin");
-    } 
+    }
   }
 });
 
 app.post("/admin", function (req, res) {
-  User.findOne({
-    username: req.body.username
-  }, function (err, user) {
-    if(user && user.role == 1){
-  const user = new User({
-    username: req.body.username,
-    password: req.body.password,
-  });
-  req.login(user, function (err) {
-    if (err) {
-      console.log(err);
-      res.redirect("/");
-    } else {
-      passport.authenticate("local")(req, res, function () {
-        res.redirect("/admin/dashboard");
-      });
-    }
-  });
-} else {
-  res.redirect("/admin");
-}
-});
-});
-
-app.post("/register", function (req, res) {
-  if (req.isAuthenticated() && req.user.role == 1){
-
-  User.register(
+  User.findOne(
     {
       username: req.body.username,
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      role: 0,
-      file_path: [],
     },
-    req.body.password,
     function (err, user) {
-      if (err) {
-        console.log(err);
-        res.redirect('/admin/dashboard');
+      if (user && user.role == 1) {
+        const user = new User({
+          username: req.body.username,
+          password: req.body.password,
+        });
+        req.login(user, function (err) {
+          if (err) {
+            console.log(err);
+            res.redirect("/");
+          } else {
+            passport.authenticate("local")(req, res, function () {
+              res.redirect("/admin/dashboard");
+            });
+          }
+        });
       } else {
-        res.redirect('/admin/dashboard');
+        res.redirect("/admin");
       }
     }
   );
-} else {
-  res.redirect("/admin");
-}
+});
+
+app.post("/register", function (req, res) {
+  if (req.isAuthenticated() && req.user.role == 1) {
+    User.register(
+      {
+        username: req.body.username,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        role: 0,
+        file_path: [],
+      },
+      req.body.password,
+      function (err, user) {
+        if (err) {
+          console.log(err);
+          res.redirect("/admin/dashboard");
+        } else {
+          res.redirect("/admin/dashboard");
+        }
+      }
+    );
+  } else {
+    res.redirect("/admin");
+  }
 });
 
 app.post("/login", function (req, res) {
-  User.findOne({
-    username: req.body.username
-  }, function (err, user) {
-    if(user && user.role == 0){
-  const user = new User({
-    username: req.body.username,
-    password: req.body.password,
-  });
-  req.login(user, function (err) {
-    if (err) {
-      console.log(err);
-      res.redirect("/");
-    } else {
-      passport.authenticate("local")(req, res, function () {
-        res.redirect("/dashboard");
-      });
+  User.findOne(
+    {
+      username: req.body.username,
+    },
+    function (err, user) {
+      if (user && user.role == 0) {
+        const user = new User({
+          username: req.body.username,
+          password: req.body.password,
+        });
+        req.login(user, function (err) {
+          if (err) {
+            console.log(err);
+            res.redirect("/");
+          } else {
+            passport.authenticate("local")(req, res, function () {
+              res.redirect("/dashboard");
+            });
+          }
+        });
+      } else {
+        res.redirect("/");
+      }
     }
-  });
-} else {
-  res.redirect("/");
-}
-});
+  );
 });
 
 app.get("/logout", function (req, res) {
   req.logout();
-  req.session.destroy(function() {
-    res.clearCookie('connect.sid');
-    res.redirect('/');
-});
+  req.session.destroy(function () {
+    res.clearCookie("connect.sid");
+    res.redirect("/");
+  });
 });
 
 let port = process.env.PORT;
